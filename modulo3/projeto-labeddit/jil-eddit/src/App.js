70c63ba6be54c800a699559a04b8653769e2bef7
Jil Mayumi Moutinho <jilmayumieu@hotmail.com>
2022-07-25 17:30:54 -0300

    Inicia projeto - cria paginas

---
+++ modulo3/projeto-labeddit/jil-eddit/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from './routes/Router'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+      <Router/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/projeto-labeddit/jil-eddit/src/pages/FeedPage/FeedPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeedPage = () => {
+  return <div>Feed</div>;
+};
+
+export default FeedPage;
