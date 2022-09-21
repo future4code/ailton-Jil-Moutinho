2bbe99466cb6cf6ae1533d809b47ffd09baed581
Jil Mayumi Moutinho <jilmayumieu@hotmail.com>
2022-09-21 14:00:50 -0300

    Adiciona exercicios do dia

---
+++ modulo5/arquitetura-injecao/src/router/userRouter.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { UserController } from "../controller/UserController";
+import { UserDatabase } from "../database/UserDatabase";
+import { Authenticator } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
+
+export const userRouter = Router();
+
+/* instanciar a classe e todas as dependencias do endpoint de user */
+const userController = new UserController(
+  new UserBusiness(
+    new UserDatabase(),
+    new IdGenerator(),
+    new HashManager(),
+    new Authenticator()
+  )
+);
+
+userRouter.post("/signup", userController.signup);
+userRouter.post("/login", userController.login);
+userRouter.get("/", userController.getUsers);
+userRouter.delete("/:id", userController.deleteUser);
+userRouter.put("/:id", userController.editUser);
